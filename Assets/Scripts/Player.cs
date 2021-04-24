92b17c5f319edbbcd9b48e35aa8a5ee4e884919e
bladeyoon <duyoungyoon@gmail.com>
2021-04-24 09:04:33 -0700

    Added Simple Player Movement
    Added Simple Player Movement
    WASD Key

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //take current position = new transform.position (0,0,0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            //transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime);
+        }
+    }
+}
