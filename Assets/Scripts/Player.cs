0a9a575ff7e6a2d4e780e1623ea1eae12d33ead1
bladeyoon <duyoungyoon@gmail.com>
2021-04-25 21:35:46 -0700

    Created player movement restriction and laser prefab
    Created player movement restriction and laser prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,37 +4,82 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 3.5f;
+    private float _speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        //take current position = new transform.position (0,0,0)
+        //make current position = new transform.position (0,0,0)
         transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        // using InputManager system
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        /*
+        if (transform.position.y >= 4)
         {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
-            //transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, 4, transform.position.z);
         }
 
-        if (Input.GetKey(KeyCode.A))
+        else if (transform.position.y <= -4)
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, -4, transform.position.z);
+        }
+        */
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4, 4), transform.position.z);
+
+        if (transform.position.x >= 4)
+        {
+            transform.position = new Vector3(-4, transform.position.y, transform.position.z);
         }
 
-        if (Input.GetKey(KeyCode.W))
+        else if (transform.position.x <= -4)
         {
-            transform.Translate(Vector3.up * speed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.down * Time.deltaTime);
+            transform.position = new Vector3(4, transform.position.y, transform.position.z);
         }
     }
 }
+
+
+// Move an object to right direction in real time
+//transform.Translate(Vector3.right * _speed * Time.deltaTime);
+
+//Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+//transform.Translate(direction * _speed * Time.deltaTime);
+
+/*
+if (Input.GetKey(KeyCode.D))
+{ 
+    transform.Translate(Vector3.right * speed * Time.deltaTime);
+    //transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime);
+}
+
+if (Input.GetKey(KeyCode.A))
+{
+    transform.Translate(Vector3.left * speed * Time.deltaTime);
+}
+
+if (Input.GetKey(KeyCode.W))
+{
+    transform.Translate(Vector3.up * speed * Time.deltaTime);
+}
+
+if (Input.GetKey(KeyCode.S))
+{
+    transform.Translate(Vector3.down * speed * Time.deltaTime);
+}
+*/
